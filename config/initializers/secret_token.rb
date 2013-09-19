01fa1b623ae9e7607e1ee828be5ed452050ec4f3
Zach Loubier <zach@muvdigital.com>
2013-09-19 17:52:13 -0400

    created products model, added test data, added styling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '90e6f20d686388333655bdf70ec486388c7a2dbc7e5e47e1b783e643ef1f16177a424141515ca997c96d31bd85a63b1ea969eaf3a6af1c36d10c32d974682561'
